bd3c08da11c2b7908ff355c8c98fc9800140dace
neo773 <neo773@protonmail.com>
2025-04-07 07:06:20 +0530

    WIP

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,25 +2,37 @@ import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { PrismaClient } from "@prisma/client";
 import { magicLink } from "better-auth/plugins/magic-link";
+import { Resend } from "resend";
+import MacGamingDBMagicLinkEmail from "@/react-email-starter/emails/magic-link";
 
 const prisma = new PrismaClient();
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
-    provider: "sqlite", 
+    provider: "sqlite",
   }),
-  emailAndPassword: {  
-    enabled: true
-},
+  emailAndPassword: {
+    enabled: true,
+  },
   plugins: [
     magicLink({
-      sendMagicLink: async ({ email, token, url }, request) => {
-          // send email to user
-          console.log(email, token, url);
-          
-      }
-  })
+      sendMagicLink: async ({ email, token, url }) => {
+        console.log(`Sending magic link to ${email} with token ${token} and url ${url}`);
+
+        if (process.env.NODE_ENV !== "production") {
+          return;
+        }
+        const resend = new Resend(process.env.RESEND_API_KEY);
+
+        await resend.emails.send({
+          from: "MacGamingDB <hello@macgamingdb.com>",
+          to: email,
+          subject: "Log in to MacGamingDB with this magic link",
+          react: MacGamingDBMagicLinkEmail({ magicLink: url }),
+        });
+      },
+    }),
   ],
 });
 
-export type Auth = typeof auth; 
+export type Auth = typeof auth;
