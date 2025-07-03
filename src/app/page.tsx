b462bd8c265517e5acb5804382c9ddaf5a52f305
neo773 <neo773@protonmail.com>
2025-07-03 18:37:05 +0530

    feat: replace TrafficSourceWidget with StopKillingGamesBanner in Home component for updated promotional display

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Footer from "@/components/shared/Footer";
 import Header from "@/components/shared/Header";
-// import TrafficSourceWidget from "@/components/shared/TrafficSourceBanner";
+import StopKillingGamesBanner from "@/components/shared/StopKillingGamesBanner";
 import HomeClient from "./home-client";
 import { createServerHelpers } from "@/lib/trpc/server";
 import { SearchURLParamsKeys, createFilterConfig, PlayMethodFilter } from "@/lib/constants";
@@ -28,7 +28,7 @@ export default async function Home({
 
   return (
     <div className="min-h-screen flex flex-col">
-      {/* <TrafficSourceWidget /> */}
+      <StopKillingGamesBanner />
       <Header />
 
       <p className="text-xl md:text-3xl text-gray-300 mb-8 mx-6 text-center font-medium md:mx-auto">
