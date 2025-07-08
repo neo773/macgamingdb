ac4a73258f5aacaaeab2e1642a17ebb1882e43ea
neo773 <neo773@protonmail.com>
2025-07-09 03:30:19 +0530

    refactor email: fixed ts issues, switched to Tailwind and fixed dark mode

--- src/react-email-starter/emails/magic-link.tsx
+++ src/react-email-starter/emails/magic-link.tsx
@@ -10,12 +10,13 @@ import {
   Preview,
   Section,
   Text,
+  Tailwind,
 } from "@react-email/components";
+import { tailwindConfig } from "../tailwind.config";
 
 interface MacGamingDBMagicLinkEmailProps {
   magicLink?: string;
 }
-
 
 export const MacGamingDBMagicLinkEmail = ({
   magicLink,
@@ -23,35 +24,56 @@ export const MacGamingDBMagicLinkEmail = ({
   <Html>
     <Head />
     <Preview>Log in to MacGamingDB with this magic link</Preview>
-    <Body style={main}>
-      <Container style={container}>
-        <Heading style={heading}>Login to 🎮 MacGamingDB</Heading>
-        <Section style={buttonContainer}>
-          <Button
-            style={button}
-            href={magicLink || "https://macgamingdb.app/login"}
+    <Tailwind config={tailwindConfig}>
+      <Body
+        className="bg-dark font-sans"
+        /**
+         * Override dark mode background colors
+         * https://litmus.com/community/snippets/267-override-dark-mode-background-colors-annett-forcier
+         */
+        style={{
+          background: "linear-gradient(#000000, #000000)",
+          backgroundColor: "#000000",
+        }}
+      >
+        <Container className="mx-auto py-5 pb-12 max-w-xl px-3">
+          <Heading className="text-2xl tracking-tight leading-tight font-bold text-white pt-4 my-10">
+            Login to 🎮 MacGamingDB
+          </Heading>
+          <Section className="py-7">
+            <Button
+              className="bg-primary rounded font-semibold text-white text-sm no-underline text-center block py-3 px-6"
+              href={magicLink || "https://macgamingdb.app/login"}
+            >
+              Log in with this magic link
+            </Button>
+          </Section>
+          <Text className="my-0 mb-4 text-sm leading-relaxed text-white">
+            Or, copy this link to your browser:
+          </Text>
+          <code className="font-mono font-bold p-3 bg-gray-dark tracking-tight text-sm rounded text-white border border-gray-medium block break-all">
+            {magicLink}
+          </code>
+          <br />
+          <Text className="my-0 mb-4 text-sm leading-relaxed text-white">
+            If you didn&apos;t try to login to share your game experience, you
+            can safely ignore this email.
+          </Text>
+          <Text className="my-0 mb-4 text-sm leading-relaxed text-white">
+            After logging in, you'll be able to add your experience reports for
+            Mac games.
+          </Text>
+          <Hr className="border-gray-medium my-11 mt-7" />
+          <Link
+            href="https://macgamingdb.app"
+            className="text-xs text-gray-light"
           >
-            Log in with this magic link
-          </Button>
-        </Section>
-        <Text style={paragraph}>Or, copy this link to your browser:</Text>
-        <code style={code}>{magicLink}</code>
-        <br />
-        <Text style={paragraph}>
-          If you didn&apos;t try to login to share your game experience, you can
-          safely ignore this email.
-        </Text>
-        <Text style={paragraph}>
-          After logging in, you'll be able to add your experience reports for
-          Mac games.
-        </Text>
-        <Hr style={hr} />
-        <Link href="https://macgamingdb.app" style={reportLink}>
-          MacGamingDB.app, the community-driven database for gaming experiences
-          on Mac.
-        </Link>
-      </Container>
-    </Body>
+            MacGamingDB.app, the community-driven database for gaming
+            experiences on Mac.
+          </Link>
+        </Container>
+      </Body>
+    </Tailwind>
   </Html>
 );
 
@@ -60,78 +82,3 @@ MacGamingDBMagicLinkEmail.PreviewProps = {
 } as MacGamingDBMagicLinkEmailProps;
 
 export default MacGamingDBMagicLinkEmail;
-
-const main = {
-  backgroundColor: "#000000",
-  fontFamily:
-    '-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif',
-};
-
-const container = {
-  margin: "0 auto",
-  padding: "20px 0 48px",
-  maxWidth: "560px",
-  paddingLeft: "12px",
-  paddingRight: "12px",
-};
-
-const heading = {
-  fontSize: "24px",
-  letterSpacing: "-0.5px",
-  lineHeight: "1.3",
-  fontWeight: "bold",
-  color: "#ffffff",
-  padding: "17px 0 0",
-  margin: "40px 0",
-};
-
-const paragraph = {
-  margin: "0 0 15px",
-  fontSize: "14px",
-  lineHeight: "1.4",
-  color: "#ffffff",
-  fontFamily:
-    "-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif",
-};
-
-const buttonContainer = {
-  padding: "27px 0 27px",
-};
-
-const button = {
-  backgroundColor: "#3b82f6",
-  borderRadius: "5px",
-  fontWeight: "600",
-  color: "#ffffff",
-  fontSize: "14px",
-  textDecoration: "none",
-  textAlign: "center" as const,
-  display: "block",
-  padding: "12px 23px",
-};
-
-const reportLink = {
-  fontSize: "12px",
-  color: "#898989",
-  fontFamily:
-    "-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif",
-};
-
-const hr = {
-  borderColor: "#333333",
-  margin: "42px 0 26px",
-};
-
-const code = {
-  fontFamily: "monospace",
-  fontWeight: "700",
-  padding: "12px",
-  backgroundColor: "#272727",
-  letterSpacing: "-0.3px",
-  fontSize: "14px",
-  borderRadius: "5px",
-  color: "#ffffff",
-  border: "1px solid #333333",
-  display: "block",
-  wordBreak: "break-all" as const,
-};
---
+++ src/react-email-starter/tailwind.config.ts
@@ -0,0 +1,17 @@
+import type { TailwindConfig } from "@react-email/tailwind";
+
+export const tailwindConfig = {
+  theme: {
+    extend: {
+      colors: {
+        primary: "#3b82f6",
+        dark: "#000000",
+        gray: {
+          light: "#898989",
+          medium: "#333333",
+          dark: "#272727",
+        },
+      },
+    },
+  },
+} satisfies TailwindConfig;
