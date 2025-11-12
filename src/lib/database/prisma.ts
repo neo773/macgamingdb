9ccb670bb6f55be8030d117e899c120e24161005
neo773 <neo773@protonmail.com>
2025-11-12 05:50:42 +0530

    commits to be reverted

--- src/lib/database/prisma.ts
+++ src/lib/database/prisma.ts
@@ -2,14 +2,12 @@ import { PrismaClient } from '@prisma/client';
 import { PrismaLibSQL } from '@prisma/adapter-libsql';
 
 export function createPrismaClient(): PrismaClient {
-  return process.env.NODE_ENV === 'production'
-    ? new PrismaClient({
-        adapter: new PrismaLibSQL({
-          url: `${process.env.LIBSQL_DATABASE_URL}`,
-          authToken: `${process.env.LIBSQL_DATABASE_TOKEN}`,
-        }),
-      })
-    : new PrismaClient({
-        log: [{ emit: 'event', level: 'query' }],
-      });
+  console.log(process.env.LIBSQL_DATABASE_URL);
+  console.log(process.env.LIBSQL_DATABASE_TOKEN);
+  return new PrismaClient({
+    adapter: new PrismaLibSQL({
+      url: `${process.env.LIBSQL_DATABASE_URL}`,
+      authToken: `${process.env.LIBSQL_DATABASE_TOKEN}`,
+    }),
+  });
 }
